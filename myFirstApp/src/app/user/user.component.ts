d8642034544e4468e2fdaf21bb99f617a424c7d0
Gearted <63016907+Gearted@users.noreply.github.com>
2022-10-24 12:10:20 +0200

    update user

---
+++ myFirstApp/src/app/user/user.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  
+  name = 'Doe';
+  firstName = 'John';
+  age = 25;
+  quote = '';
+  photo = 'https://randomuser.me/api/portraits/lego/2.jpg';
+
+  onKeyDownEvent(event: any){
+    this.quote = event.target.value;
+  }
+
+  isDisplay = false;
+  
+  toggleDisplay() {
+    this.isDisplay = !this.isDisplay;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
